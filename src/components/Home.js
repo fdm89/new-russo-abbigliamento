c60cd3755cfaa26a056aa769a659b80a15fee6fa
fulvio di maio <fulvio1989@gmail.com>
2023-06-16 18:03:58 +0200

    added video

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,17 +12,11 @@ import { posthog } from 'posthog-js';
 import video from '../assets/man_-_62550 (1080p).mp4';
 import CarouselHome from "./CarouselHome";
 
-const homeContainerStyle = {
-  width: '100%',
-  height: '100vh',
-  position: 'relative',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-};
+
 
 function Home() {
   const homeRef = useRef(null);
+  
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "instant" });
@@ -44,9 +38,9 @@ function Home() {
       </a>
       {posthog.has_opted_out_capturing() || posthog.has_opted_in_capturing() ? null : <CookieBanner />}
 
-      <div  style={homeContainerStyle}>
-        <video className="background-video" src={video} autoPlay loop muted />
-      </div>
+      
+        <video className="background-video" src={video} autoPlay loop muted/>
+      
 
       <div className="banner" id="banner">
         <p>Outerwear</p>
