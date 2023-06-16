f2f3c3df339b4f2b8c187d2309570a83c67399f4
fulvio di maio <fulvio1989@gmail.com>
2023-06-16 18:18:13 +0200

    added opacity to video

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Helmet } from 'react-helmet';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons";
@@ -24,11 +24,16 @@ const homeContainerStyle = {
 
 function Home() {
   const homeRef = useRef(null);
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
   
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "instant" });
   }, []);
+
+  const handleVideoLoad = () => {
+    setIsVideoLoaded(true);
+  };
 
   return (
     <div className="home-container" ref={homeRef}>
@@ -47,7 +52,7 @@ function Home() {
       {posthog.has_opted_out_capturing() || posthog.has_opted_in_capturing() ? null : <CookieBanner />}
 
       <div  style={homeContainerStyle}>
-        <video className="background-video" src={video} autoPlay loop muted/>
+        <video onLoadedData={handleVideoLoad} className={`background-video ${isVideoLoaded ? "loaded" : ""}`} src={video} autoPlay loop muted/>
       </div>
 
       <div className="banner" id="banner">
