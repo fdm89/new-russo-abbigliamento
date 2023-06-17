d677cd8cd73f30e380a61b31a51ac674a0c67ebb
fulvio di maio <fulvio1989@gmail.com>
2023-06-17 20:57:24 +0200

    russo video added

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ import Footer from "./Footer";
 import Button from "./Button";
 import CookieBanner from "./CookieBanner";
 import { posthog } from 'posthog-js';
-import video from '../assets/man_-_62550 (1080p).mp4';
+import video from '../assets/BOZZA2.mp4';
 import CarouselHome from "./CarouselHome";
 
 const homeContainerStyle = {
@@ -52,7 +52,9 @@ function Home() {
       {posthog.has_opted_out_capturing() || posthog.has_opted_in_capturing() ? null : <CookieBanner />}
 
       <div  style={homeContainerStyle}>
+        
         <video onLoadedData={handleVideoLoad} className={`background-video ${isVideoLoaded ? "loaded" : ""}`} src={video} autoPlay loop muted/>
+        
       </div>
 
       <div className="banner" id="banner">
